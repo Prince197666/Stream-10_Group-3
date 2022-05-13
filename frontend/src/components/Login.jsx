2bdc809355b9fa31da4a9e7f80f23b57cb0fe7cd
MasaGt <MasaGt@users.noreply.github.com>
2022-05-13 17:13:18 +1200

    added components and pages

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div>
+      <form>
+        User Nname
+        <input type="text" />
+        <br />
+        Password
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
