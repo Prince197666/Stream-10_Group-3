4ed966255c92a26965f720240bb18576816b64ef
MasaGt <MasaGt@users.noreply.github.com>
2022-05-18 23:30:18 +1200

    added user registration api

--- routes/api/user.js
+++ routes/api/user.js
@@ -23,4 +23,22 @@ router.get('/analysts/', (req, res) => {
       .catch(err => res.status(404).json({ noanalystsfound: 'No analysts found' }));
 });
 
+// @route GET api/users/moderators/
+// @description add/save moderator
+// @access Public
+router.post("/moderators/", (req, res) => {
+  Moderator.create(req.body)
+    .then((moderator) => res.json({ msg: "Moderator added successfully" }))
+    .catch((err) => res.status(400).json({ error: "Unable to add this moderator" }));
+});
+
+// @route GET api/users/analysts/
+// @description add/save analyst
+// @access Public
+router.post("/analysts/", (req, res) => {
+  Analyst.create(req.body)
+    .then((analyst) => res.json({ msg: "Analyst added successfully" }))
+    .catch((err) => res.status(400).json({ error: "Unable to add this analyst" }));
+});
+
 module.exports = router;
