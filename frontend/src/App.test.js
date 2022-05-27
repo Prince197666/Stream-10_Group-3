0634fecdebc02107ad2aa23bf897b53ba782df10
MasaGt <MasaGt@users.noreply.github.com>
2022-05-28 00:56:18 +1200

    supressed App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,10 @@
-import { render, screen } from '@testing-library/react';
+/* eslint-disable react/react-in-jsx-scope */
+import { render } from '@testing-library/react';
 import App from './App';
 
+// eslint-disable-next-line no-undef
 test('renders learn react link', () => {
+  // eslint-disable-next-line react/jsx-filename-extension
   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
