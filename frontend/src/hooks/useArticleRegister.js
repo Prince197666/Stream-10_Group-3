c162b37bb8ec7e2a37fc7dff4c50543a6aed75e7
MasaGt <MasaGt@users.noreply.github.com>
2022-05-31 02:51:36 +1200

    implemented email notification

--- frontend/src/hooks/useArticleRegister.js
+++ frontend/src/hooks/useArticleRegister.js
@@ -1,21 +1,41 @@
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-function useFileRegister() {
+function useArticleRegister() {
   const navigate = useNavigate();
 
-  const udpateArticle = (id, data, role) => {
+  const udpateArticle = (id, data, role, notification) => {
     axios
       .put(`http://localhost:8082/api/articles/id/${id}`, data)
-      .then((res) => {
-        console.log(`update success: ${res}`);
+      .then(() => {
         if (role === 'moderators') {
+          // send e-mail notification to analysts
+          if (notification.length > 0) {
+            axios.post('http://localhost:8082/api/email/analyst', notification)
+              .then()
+              .catch();
+          }
+
           navigate('/Moderate');
         } else {
+          if (data.status === 'register' && notification) {
+            // send e-mail notification of acceptance to a user
+            const sendData = { to: notification, title: data.title };
+            axios.post('http://localhost:8082/api/email/accept', sendData)
+              .then()
+              .catch();
+          } else if (data.status === 'reject' && notification) {
+            const sendData = { to: notification, title: data.title };
+            // send e-mail notification of reject to a user
+            axios.post('http://localhost:8082/api/email/reject', sendData)
+              .then()
+              .catch();
+          }
           navigate('/Analyse');
         }
       })
       .catch((err) => {
+        // eslint-disable-next-line no-console
         console.log(`Error in UpdateArticleInfo!: ${err}`);
       });
   };
@@ -25,4 +45,4 @@ function useFileRegister() {
   };
 }
 
-export default useFileRegister;
+export default useArticleRegister;
